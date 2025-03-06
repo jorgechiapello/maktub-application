03187ef29dd7f3b3b61c8a31cc3d3c36673227a9
jorgechiapello <jorgechiapello@gmail.com>
2025-03-06 21:02:08 +0100

    Comment out questions to make onboarding faster

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -36,44 +36,44 @@ export const preferenceQuestions: Question[] = [
       "Explorando nuevos restaurantes y bares"
     ]
   },
-  {
-    id: 3,
-    question: "¿Cómo manejas los conflictos en una relación?",
-    category: 'preference',
-    options: [
-      "Prefiero hablarlo de inmediato y resolverlo",
-      "Me tomo un tiempo para procesarlo antes de discutirlo",
-      "Evitando discusiones innecesarias"
-    ]
-  },
-  {
-    id: 4,
-    question: "¿Qué rol prefieres en una relación?",
-    category: 'preference',
-    options: [
-      "Me gusta tomar la iniciativa y sorprender",
-      "Prefiero que todo sea equilibrado y mutuo",
-      "Me encanta que me conquisten y me hagan sentir especial"
-    ]
-  },
-  {
-    id: 5,
-    question: "¿Cómo te sientes respecto a las demostraciones de afecto en público?",
-    category: 'preference',
-    options: [
-      "Me encantan, soy muy expresivo/a",
-      "Depende del contexto y la persona",
-      "Prefiero la discreción y los gestos más íntimos"
-    ]
-  },
-  {
-    id: 6,
-    question: "¿Cómo te sientes respecto al compromiso?",
-    category: 'preference',
-    options: [
-      "Me gusta la estabilidad y construir algo sólido",
-      "Prefiero que las cosas fluyan sin presiones",
-      "Me asusta un poco, pero si es la persona correcta, lo intento"
-    ]
-  }
+  // {
+  //   id: 3,
+  //   question: "¿Cómo manejas los conflictos en una relación?",
+  //   category: 'preference',
+  //   options: [
+  //     "Prefiero hablarlo de inmediato y resolverlo",
+  //     "Me tomo un tiempo para procesarlo antes de discutirlo",
+  //     "Evitando discusiones innecesarias"
+  //   ]
+  // },
+  // {
+  //   id: 4,
+  //   question: "¿Qué rol prefieres en una relación?",
+  //   category: 'preference',
+  //   options: [
+  //     "Me gusta tomar la iniciativa y sorprender",
+  //     "Prefiero que todo sea equilibrado y mutuo",
+  //     "Me encanta que me conquisten y me hagan sentir especial"
+  //   ]
+  // },
+  // {
+  //   id: 5,
+  //   question: "¿Cómo te sientes respecto a las demostraciones de afecto en público?",
+  //   category: 'preference',
+  //   options: [
+  //     "Me encantan, soy muy expresivo/a",
+  //     "Depende del contexto y la persona",
+  //     "Prefiero la discreción y los gestos más íntimos"
+  //   ]
+  // },
+  // {
+  //   id: 6,
+  //   question: "¿Cómo te sientes respecto al compromiso?",
+  //   category: 'preference',
+  //   options: [
+  //     "Me gusta la estabilidad y construir algo sólido",
+  //     "Prefiero que las cosas fluyan sin presiones",
+  //     "Me asusta un poco, pero si es la persona correcta, lo intento"
+  //   ]
+  // }
 ];
