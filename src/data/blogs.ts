c128a86b4220ed45fbbaeb53920c8170586dc1b6
jorgechiapello <jorgechiapello@gmail.com>
2025-03-09 13:33:43 +0100

    Extract blogs data into filw

---
+++ src/data/blogs.ts
@@ -0,0 +1,29 @@
+export const blogArtOfConversation = {
+  title: 'El arte de la conversación: Estrategias de citas para Maktub',
+  categoryTitle: 'Estrategias de citas',
+  categoryId: 'dating-strategies',
+  image: '/wp-content/uploads/sites/128/2025/03/tenweb_media_rx9hefrca.webp',
+  postId: 'the-art-of-conversation-dating-strategies-for',
+  excerpt:
+    'Domina el arte de la conversación y crea conexiones duraderas en Maktub.',
+}
+
+export const blogUnlockingLove = {
+  title: 'Descubriendo el Amor en Barcelona: Tu Guía de AmorLounge',
+  categoryTitle: 'Consejos de citas',
+  categoryId: 'dating-tips',
+  image: '/wp-content/uploads/sites/128/2025/03/tenweb_media_R7ZFYALQF.webp',
+  postId: 'unlocking-love-in-barcelona-your-guide-to',
+  excerpt:
+    'AmorLounge es más que un bar; es una experiencia. Sumérgete en la atmósfera vibrante, explora nuestras salas temáticas y descubre la conexión perfecta.',
+}
+
+export const blogEvents = {
+  title: 'El lugar más vibrante de Barcelona: Eventos Maktub',
+  categoryTitle: 'Eventos',
+  categoryId: 'events',
+  image: '/wp-content/uploads/sites/128/2025/03/tenweb_media_rrzsflcu0.webp',
+  postId: 'barcelonas-hottest-spot-amorlounge-events',
+  excerpt:
+    'Vive la vibrante vida nocturna y los eventos exclusivos de Maktub, el bar de citas líder en Barcelona.'
+}
