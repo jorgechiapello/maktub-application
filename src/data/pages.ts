e9dffac1a46bfb95470f55c7c7967ee7078a1172
jorgechiapello <jorgechiapello@gmail.com>
2025-03-09 17:22:17 +0100

    Render navigation from pages object

--- src/data/pages.ts
+++ src/data/pages.ts
@@ -1,17 +1,44 @@
 import { blogCategories, blogPosts } from '@data/blogs'
 
-const blogs = blogPosts.reduce((acc, post) => ({ ...acc, [post.postId]: post.title }), {})
+const blogsPathsAndTitles = blogPosts.reduce(
+  (acc, post) => ({ ...acc, [`/${post.postId}`]: post.title }),
+  {},
+)
 
-export const pages: Record<string, string> = {
-  '': 'Inicio',
-  ambientes: 'Ambientes',
-  blog: 'Blog',
-  conocenos: 'Conocenos',
-  contactanos: 'Contactanos',
-  'la-app': 'La app',
-  [blogCategories.datingStrategies.id]: blogCategories.datingStrategies.title,
-  [blogCategories.datingTips.id]: blogCategories.datingTips.title,
-  [blogCategories.events.id]: blogCategories.events.title,
-  ...blogs,
+const blogCategoriesPathsAndName = Object.entries(blogCategories).reduce(
+  (acc, [key, value]) => ({ ...acc, [`/${value.id}`]: value.title }),
+  {},
+)
+
+type Page = { label: string; path: string }
+
+export const initialPage: Page = { label: 'Inicio', path: '/' }
+export const ambientesPage: Page = { label: 'Ambientes', path: '/ambientes' }
+export const blogPage: Page = { label: 'Blog', path: '/blog' }
+export const conocenosPage: Page = { label: 'Conocenos', path: '/conocenos' }
+export const contactanosPage: Page = {
+  label: 'Contactanos',
+  path: '/contactanos',
 }
-console.log(pages)
+export const laAppPage: Page = { label: 'La app', path: '/la-app' }
+export const comeAquiPage: Page = { label: 'Come aqui', path: '/come-aqui' }
+
+export const topLevelPages = [
+  conocenosPage,
+  laAppPage,
+  blogPage,
+  ambientesPage,
+  comeAquiPage,
+  contactanosPage
+]
+
+export const pagesTitles: Record<string, string> = {
+  [initialPage.path]: initialPage.label,
+  [ambientesPage.path]: ambientesPage.label,
+  [blogPage.path]: blogPage.label,
+  [conocenosPage.path]: conocenosPage.label,
+  [contactanosPage.path]: contactanosPage.label,
+  [laAppPage.path]: laAppPage.label,
+  ...blogsPathsAndTitles,
+  ...blogCategoriesPathsAndName
+}
